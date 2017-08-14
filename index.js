1883ca5aae34ba23ca962ddef56f68c33944ca88
Jason Tuhy <jasontuhy@gmail.com>
2017-08-13 20:08:33 -0400

    Skeleton code

---
+++ index.js
@@ -0,0 +1,28 @@
+
+'use strict'
+
+const body = require('body-parser')
+const co = require('co')
+const express = require('express')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+const PORT = 3000
+
+
+co(function * () {
+
+  yield app.prepare()
+
+  const server = express()
+  server.use(body.json())
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+  server.listen(PORT)
+  console.log(`Listening on ${PORT}`)
+
+});
