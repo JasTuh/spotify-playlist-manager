ded98539c96fc2fb3b889ac5d07b2284fb1d55e0
JasTuh <jasontuhy@gmail.com>
2017-08-22 19:32:33 -0400

    Add utility to get tracks from playlist

--- pages/userLoggedIn.js
+++ pages/userLoggedIn.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
-import getPlaylists from '../utils/getPlaylists.js';
+import getPlaylists from '../utils/getPlaylists';
+import getTracksOfPlaylist from '../utils/getTracksOfPlaylist';
 
 export default class UserLoggedIn extends Component {
   constructor(props) {
@@ -14,6 +15,9 @@ export default class UserLoggedIn extends Component {
       this.setState({
         playlists: res,
       });
+    });
+    getTracksOfPlaylist("https://api.spotify.com/v1/users/1212905325/playlists/45n9rUFRfk6w9q1w96z7Fh/tracks", this.props.url.query.accessToken).then(text => {
+      console.log(text);
     });
   }
   render() {
