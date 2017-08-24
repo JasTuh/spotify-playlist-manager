fcd0e88170a13fa9de37c4d0f9c38bb2890e05b0
JasTuh <jasontuhy@gmail.com>
2017-08-24 14:00:10 -0400

    Added css file

--- components/meta.js
+++ components/meta.js
@@ -6,14 +6,7 @@ export default () => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
+      <link rel="stylesheet" type="text/css" href="/static/css/style.css" />
     </Head>
-    <style jsx global>{`
-      body { 
-        background: #000;
-        font: 11px menlo;
-        color: #fff;
-      }
-    `}</style>
   </div>
-)
-
+);
